e44b268c08713ef88991d9a22cd488210de37d92
GreyAudron <mikhail.schee@mail.utoronto.ca>
2022-01-04 12:48:31 -0500

    Copied old code to wrap many frames into a gif

---
+++ create_gif.py
@@ -0,0 +1,38 @@
+"""
+Author: Mikhail Schee
+Creates a gif of a set of png images in the stated directory.
+
+Script created: 2019/03/19, Mikhail Schee
+Last updated: 2022/01/04, Mikhail Schee
+"""
+
+"""
+Using imageio
+https://imageio.readthedocs.io/en/latest/installation.html
+install using:
+    conda install -c conda-forge imageio
+OR
+    pip install imageio
+Skeleton script from
+https://stackoverflow.com/questions/41228209/making-gif-from-images-using-imageio-in-python
+"""
+
+import os
+import imageio
+
+gif_file = 'ITP3-1073.gif'
+png_dir = 'frames'
+print('Saving gif to', gif_file)
+
+
+# To set the frames per second
+fps = 20
+frame_duration = 1.0 / fps
+
+images = []
+# need to sort because os.listdir returns a list of arbitrary order
+for file_name in sorted(os.listdir(png_dir)):
+    if file_name.endswith('.png'):
+        file_path = os.path.join(png_dir, file_name)
+        images.append(imageio.imread(file_path))
+imageio.mimsave(gif_file, images, duration=frame_duration)
